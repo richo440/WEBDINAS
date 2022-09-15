40df56d150eb3df0f205d33dd334080a2ff47dc7
richo440 <109513701+richo440@users.noreply.github.com>
2022-09-15 11:06:40 +0700

    Add files via upload

---
+++ src/Beranda.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import NavigationBar from "./components/NavigationBar"
+import Intro from "./components/Intro"
+import Trending from "./components/Trending"
+import Profil from "./components/Profil"
+import Bawah from "./components/Bawah"
+import Artikel from "./components/Artikel"
+import "./style/landingPage.css"
+
+function Beranda() {
+  return (
+    <div>
+      <div className="myBG">
+        <NavigationBar />
+        <Intro />
+      </div>
+
+      <div className="trending">
+        <Trending />
+      </div>
+
+      <div className="artikel">
+        <Artikel />
+      </div>
+
+      <div className="profil">
+        <Profil />
+      </div>
+
+      <div className="bawah">
+        <Bawah />
+        </div>
+    </div>
+    
+  )
+}
+
+export default Beranda;
